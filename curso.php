bf5567bb955a5eb7e340d61d4959096d32aa5826
Zeta Aquilae <zeta.aquilae@gmail.com>
2024-10-31 20:17:57 -0300

    introdução

--- curso.php
+++ curso.php
@@ -8,21 +8,21 @@ $error_msg = "";
 if (!isset($_SESSION['id_user'])) {
 
     if (isset($_POST['submit'])) {
-        
+
         $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
         $email = mysqli_real_escape_string($dbc, trim($_POST['email']));
         $senha = mysqli_real_escape_string($dbc, trim($_POST['senha']));
 
         if (!empty($email) && !empty($senha)) {
-           
+
 
             $query = "SELECT id_usuario, nome, senha, email FROM alunos WHERE email = '$email'";
 
             $data = mysqli_query($dbc, $query);
 
             if (mysqli_num_rows($data) == 1) {
-               
+
                 $row = mysqli_fetch_array($data);
 
                 if ($row['senha'] === SHA1($senha)) {
@@ -45,12 +45,10 @@ if (!isset($_SESSION['id_user'])) {
                 }
             } else {
                 $error_msg = 'E-mail ou nome de usuário inválidos. Tente novamente.';
-            } 
-        }  
-        else {
+            }
+        } else {
             $error_msg = 'Você precisa digitar o nome de usuário e senha para entrar.';
-        } 
-
+        }
     }
 }
 
@@ -84,37 +82,69 @@ if (!isset($_SESSION['id_user'])) {
         </header>
 
 
-            <?php
-           
-            if (empty($_SESSION['id_usuario'])) {
-                echo '<p class="texto-alerta mensagens">' . $error_msg . 'Você não está logado. <a href="login.php">Fazer login.</a></p>';
-            
-            } else {
-               
-                echo ('<p class="mensagens">Você está logado como ' . $_SESSION['email'] . '.</p>');
-                include ('nav.php');
+        <?php
 
-            ?>
+        if (empty($_SESSION['id_usuario'])) {
+            echo '<p class="texto-alerta mensagens">' . $error_msg . 'Você não está logado. <a href="login.php">Fazer login.</a></p>';
+        } else {
+
+            echo ('<p class="mensagens">Você está logado como ' . $_SESSION['email'] . '.</p>');
 
 
-        <main>
+
+        ?>
+
+            <nav>
+                <ul>
+                    <li class="modulo"><a href="">Módulo 1</a></li>
+                    <li><a href="">Aula 1</a></li>
+                    <li><a href="">Aula 2</a></li>
+                    <li><a href="">Aula 3</a></li>
+                    <li><a href="">Aula 4</a></li>
+                    <li><a href="">Aula 5</a></li>
+                    <li><a href="">Aula 6</a></li>
+                    <li><a href="">Aula 7</a></li>
+                    <li><a href="">Aula 8</a></li>
+                    <li><a href="">Aula 9</a></li>
+                    <li><a href="">Aula 10</a></li>
+                    <li class="modulo"><a href="">Módulo 2</a></li>
+                    <li class="modulo"><a href="">Módulo 3</a></li>
+                    <li class="modulo"><a href="">Módulo 4</a></li>
+                    <li class="modulo"><a href="">Módulo 5</a></li>
+                </ul>
+            </nav>
+
+            <main>
 
 
-<h1>Lição 1</h1>
+                <h3>Bem-vindo!</h3>
+                <p>Olá e bem-vindo ao primeiro passo para dominar a língua inglesa! 🎉 </p>
+                <p>Aqui, você encontrará:</p>
 
-        </main>
-            <?php
+                <p><strong>Aulas Interativas</strong>: Projetadas para envolver e motivar, com foco em habilidades práticas de gramática e vocabulário.</p>
+                <p><strong>Recursos Exclusivos</strong>: Acesso a materiais como quizzes e exercícios práticos.</p>
+                <p><strong>Flexibilidade</strong>: Estude no seu próprio ritmo e de qualquer lugar, com total suporte.</p>
+                <p><strong>Certificação</strong>: Receba um certificado ao final do curso, para dar aquele impulso no seu currículo e abrir novas oportunidades.</p>
+
+                <p>Esperamos que você aproveite cada momento e tire o máximo proveito do seu aprendizado. Estamos aqui para apoiá-lo em cada etapa do caminho. Prepare-se para transformar seu futuro com o inglês!</p>
 
 
-            }
+            </main>
+        <?php
 
-            mysqli_close($dbc);
-            ?>
+
+        }
+
+        mysqli_close($dbc);
+        ?>
 
 
 
         <footer>
-            <p id="copyright">Todos os direitos reservados <script>document.write(new Date().getFullYear());</script></p>
+            <p id="copyright">Todos os direitos reservados <script>
+                    document.write(new Date().getFullYear());
+                </script>
+            </p>
         </footer>
 
     </div>
--- nav.php
+++ nav.php
@@ -1,6 +1,17 @@
 <nav>
-<ul>
-    <li>teste</li>
-    <li>teste 2</li>
-</ul>
-</nav>
+    <ul>
+        <button class="dropdown-btn">Módulo 1<i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-container">
+            <ul>
+                <li><a href="aula1.php">Aula 1</a></li>
+                <li><a href="aula1.php">Aula 2</a></li>
+                <li><a href="aula1.php">Aula 3</a></li>
+            </ul>
+        </div>
+        <li><a href="">Módulo 1</a></li>
+        <li><a href="">Módulo 2</a></li>
+        <li><a href="">Módulo 3</a></li>
+        <li><a href="">Módulo 4</a></li>
+        <li><a href="">Módulo 5</a></li>
+    </ul>
+</nav> 
