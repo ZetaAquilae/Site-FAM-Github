f0f2bd4819bdee50b2a1a18c758424e349f274a6
Zeta Aquilae <zeta.aquilae@gmail.com>
2024-11-01 17:58:17 -0300

    lição 6

---
+++ curso/modulo1/m1-a6.php
@@ -0,0 +1,244 @@
+<?php
+require_once('../../geral/connectdb.php');
+
+session_start();
+$error_msg = "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../assets/css/main.css" rel="stylesheet" type="text/css">
+    <script src="../../assets/javascript/main.js"></script>
+    <title>Curso de Inglês</title>
+</head>
+
+<body>
+    <div id="container">
+        <header>
+            <div id="navegacao-div">
+                <div id="logo"><a href="../../index.html"><img src="../../assets/images/design/english-course-logo.png" alt="logo"></a></div>
+                <div id="curso-nav">
+                    <h1>Curso de Inglês</h1>
+                </div>
+                <div id="button-nav">
+                    <a href="../../logout.php">
+                        <button id="login-nav">Sair</button>
+                    </a>
+                </div>
+            </div>
+        </header>
+
+        <?php
+
+        if (empty($_SESSION['id_usuario'])) {
+            echo '<p class="texto-alerta mensagens">' . $error_msg . 'Você não está logado. <a href="../../login.php">Fazer login.</a></p>';
+        } else {
+
+            echo ('<p class="mensagens">Você está logado como ' . $_SESSION['email'] . '.</p>');
+
+        ?>
+
+            <nav>
+                <ul>
+                    <li class="modulo"><a href="../../curso.php">Início</a></li>
+                    <li class="modulo"><a href="m1-a1.php">Módulo 1</a></li>
+                    <li><a href="m1-a1.php">Lição 1</a></li>
+                    <li><a href="m1-a2.php">Lição 1</a></li>
+                    <li><a href="m1-a3.php">Lição 3</a></li>
+                    <li><a href="m1-a4.php">Lição 4</a></li>
+                    <li><a href="m1-a5.php">Lição 5</a></li>
+                    <li class="nav-active"><a href="m1-a6.php">Lição 6</a></li>
+                    <li><a href="m1-a7.php">Lição 7</a></li>
+                    <li><a href="m1-a8.php">Lição 8</a></li>
+                    <li><a href="m1-a9.php">Lição 9</a></li>
+                    <li><a href="m1-a10.php">Lição 10</a></li>
+                    <li class="modulo"><a href="../modulo2/m2-a1.php">Módulo 2</a></li>
+                    <li class="modulo"><a href="../modulo2/m3-a1.php">Módulo 3</a></li>
+                    <li class="modulo"><a href="../modulo2/m4-a1.php">Módulo 4</a></li>
+                    <li class="modulo"><a href="../modulo2/m5-a1.php">Módulo 5</a></li>
+                </ul>
+            </nav>
+
+            <main>
+
+                <h2>Módulo 1</h2>
+                <h3>Lição 6 - Na Escola</h3>
+
+                <h4>Objetivo:</h4>
+
+                <p>Aprender vocabulário e expressões relacionadas ao ambiente escolar.</p>
+
+                <h4>Conteúdo:</h4>
+
+                <h4>1. Vocabulário Comum:</h4>
+
+                <p>Teacher (professor)</p>
+
+                <p>Student (estudante)</p>
+
+                <p>Classroom (sala de aula)</p>
+
+                <p>Desk (escrivaninha)</p>
+
+                <p>Notebook (caderno)</p>
+
+                <p>Pen (caneta)</p>
+
+                <p>Pencil (lápis)</p>
+
+                <p>Blackboard (quadro negro)</p>
+
+                <p>Lesson (lição)</p>
+
+                <p>Homework (dever de casa)</p>
+
+                <h4>2. Frases Úteis:</h4>
+
+                <p>What is your favorite subject? (Qual é a sua matéria favorita?)</p>
+
+                <p>I have math class. (Eu tenho aula de matemática.)</p>
+
+                <p>Can I borrow a pencil? (Posso pegar um lápis emprestado?)</p>
+
+                <p>The teacher is explaining the lesson. (O professor está explicando a lição.)</p>
+
+                <p>Did you do your homework? (Você fez seu dever de casa?)</p>
+
+                <h4>3. Exemplo de Diálogo:</h4>
+
+                <p>Student A: What is your favorite subject? <br>
+                    Student B: My favorite subject is English. What about you? <br>
+                    Student A: I like math. <br>
+                    Student B: Can I borrow a pencil? I forgot mine. <br>
+                    Student A: Sure, here you go. Did you do your homework? <br>
+                    Student B: Yes, I finished it last night.</p>
+                <br><br>
+                <hr>
+                <h2>Exercícios</h2>
+
+                <?php
+
+                $Questions = array(
+                    1 => array(
+                        'Question' => 'The teacher is explaining the _______.',
+                        'Answers' => array(
+                            'A' => 'desk',
+                            'B' => 'lesson',
+                            'C' => 'pen'
+                        ),
+                        'CorrectAnswer' => 'B'
+                    ),
+                    2 => array(
+                        'Question' => 'Can I borrow a _______?',
+                        'Answers' => array(
+                            'A' => 'student',
+                            'B' => 'teacher',
+                            'C' => 'pencil'
+                        ),
+                        'CorrectAnswer' => 'C'
+                    ),
+                    3 => array(
+                        'Question' => 'What is your favorite _______?',
+                        'Answers' => array(
+                            'A' => 'subject',
+                            'B' => 'homework',
+                            'C' => 'classroom'
+                        ),
+                        'CorrectAnswer' => 'A'
+                    ),
+                    4 => array(
+                        'Question' => 'I have _______ class.',
+                        'Answers' => array(
+                            'A' => 'desk',
+                            'B' => 'math',
+                            'C' => 'pen'
+                        ),
+                        'CorrectAnswer' => 'B'
+                    ),
+                    5 => array(
+                        'Question' => 'Did you do your _______?',
+                        'Answers' => array(
+                            'A' => 'homework',
+                            'B' => 'notebook',
+                            'C' => 'blackboard'
+                        ),
+                        'CorrectAnswer' => 'A'
+                    )
+                );
+
+                if (isset($_POST['answers'])) {
+                    $Answers = $_POST['answers'];
+
+                    foreach ($Questions as $QuestionNo => $Value) {
+
+                        echo $Value['Question'] . '<br />';
+
+                        if ($Answers[$QuestionNo] != $Value['CorrectAnswer']) {
+                            echo 'Você respondeu: <span class="incorrect">' . $Value['Answers'][$Answers[$QuestionNo]] . '</span><br>';
+                            echo 'Resposta correta: <span class="correct">' . $Value['Answers'][$Value['CorrectAnswer']] . '</span>';
+                        } else {
+                            echo 'Resposta correta: <span class="correct">' . $Value['Answers'][$Answers[$QuestionNo]] . '</span><br>';
+                            echo 'Você acertou!';
+                            $counter++;
+                        }
+
+                        echo '<br /><hr>';
+                        if ($counter == "") {
+                            $counter = '0';
+                            $results = "Pontuação: $counter/5";
+                        } else {
+                            $results = "Pontuação: $counter/5";
+                        }
+                    }
+                    echo $results;
+                } else {
+                ?>
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="quiz">
+                        <?php foreach ($Questions as $QuestionNo => $Value) { ?>
+
+                            <p class="pergunta"><?php echo $Value['Question']; ?></p>
+                            <?php
+                            foreach ($Value['Answers'] as $Letter => $Answer) {
+                                $Label = 'question-' . $QuestionNo . '-answers-' . $Letter;
+                            ?>
+                                <div class="opcoes">
+                                    <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter; ?>" />
+                                    <label for="<?php echo $Label; ?>"><?php echo $Letter; ?>) <?php echo $Answer; ?> </label>
+                                </div>
+                            <?php } ?>
+
+                        <?php } ?>
+                        <input type="submit" value="Verificar" class="botao-verificar" />
+                    </form>
+                <?php
+                }
+                ?>
+
+                <div class="proximo">
+                    <p><a href="m1-a7.php" class="proximo-link">Ir para a lição 7</a></p>
+                </div>
+
+            </main>
+        <?php
+
+        }
+
+        mysqli_close($dbc);
+        ?>
+
+        <footer>
+            <p id="copyright">Todos os direitos reservados <script>
+                    document.write(new Date().getFullYear());
+                </script>
+            </p>
+        </footer>
+
+    </div>
+</body>
+
+</html>
