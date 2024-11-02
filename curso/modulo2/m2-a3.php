ba8750e7cd50dfd705297b8571716175a3f413d7
Zeta Aquilae <zeta.aquilae@gmail.com>
2024-11-02 10:24:10 -0300

    lição 13

---
+++ curso/modulo2/m2-a3.php
@@ -0,0 +1,238 @@
+<?php
+require_once('../../geral/connectdb.php');
+
+session_start();
+$error_msg = "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../assets/css/main.css" rel="stylesheet" type="text/css">
+    <script src="../../assets/javascript/main.js"></script>
+    <title>Curso de Inglês</title>
+</head>
+
+<body>
+    <div id="container">
+        <header>
+            <div id="navegacao-div">
+                <div id="logo"><a href="../../index.html"><img src="../../assets/images/design/english-course-logo.png" alt="logo"></a></div>
+                <div id="curso-nav">
+                    <h1>Curso de Inglês</h1>
+                </div>
+                <div id="button-nav">
+                    <a href="../../logout.php">
+                        <button id="login-nav">Sair</button>
+                    </a>
+                </div>
+            </div>
+        </header>
+
+        <?php
+
+        if (empty($_SESSION['id_usuario'])) {
+            echo '<p class="texto-alerta mensagens">' . $error_msg . 'Você não está logado. <a href="../../login.php">Fazer login.</a></p>';
+        } else {
+
+            echo ('<p class="mensagens">Você está logado como ' . $_SESSION['email'] . '.</p>');
+
+        ?>
+
+            <nav>
+                <ul>
+                    <li class="modulo"><a href="../../curso.php">Início</a></li>
+                    <li class="modulo"><a href="../modulo1/m1-a1.php">Módulo 1</a></li>
+
+                    <li class="modulo"><a href="m2-a1.php">Módulo 2</a></li>
+                    <li><a href="m2-a1.php">Lição 11</a></li>
+                    <li><a href="m2-a2.php">Lição 12</a></li>
+                    <li class="nav-active"><a href="m2-a3.php">Lição 13</a></li>
+                    <li><a href="m2-a4.php">Lição 14</a></li>
+                    <li><a href="m2-a5.php">Lição 15</a></li>
+                    <li><a href="m2-a6.php">Lição 16</a></li>
+                    <li><a href="m2-a7.php">Lição 17</a></li>
+                    <li><a href="m2-a8.php">Lição 18</a></li>
+                    <li><a href="m2-a9.php">Lição 19</a></li>
+                    <li><a href="m2-a10.php">Lição 20</a></li>
+                    <li class="modulo"><a href="../modulo2/m3-a1.php">Módulo 3</a></li>
+                    <li class="modulo"><a href="../modulo2/m4-a1.php">Módulo 4</a></li>
+                    <li class="modulo"><a href="../modulo2/m5-a1.php">Módulo 5</a></li>
+                </ul>
+            </nav>
+
+            <main>
+
+                <h2>Módulo 2</h2>
+                <h3>Lição 13 - Dias da Semana</h3>
+
+                <h4>Objetivo:</h4>
+
+                <p>Aprender os nomes dos dias da semana em inglês e como usá-los em frases.</p>
+
+                <h4>Conteúdo:</h4>
+
+                <h4>1. Nomes dos Dias da Semana:</h4>
+
+                <p>Monday (segunda-feira)</p>
+
+                <p>Tuesday (terça-feira)</p>
+
+                <p>Wednesday (quarta-feira)</p>
+
+                <p>Thursday (quinta-feira)</p>
+
+                <p>Friday (sexta-feira)</p>
+
+                <p>Saturday (sábado)</p>
+
+                <p>Sunday (domingo)</p>
+
+                <h4>2. Frases Úteis:</h4>
+
+                <p>Today is Monday. (Hoje é segunda-feira.)</p>
+
+                <p>Tomorrow is Tuesday. (Amanhã é terça-feira.)</p>
+
+                <p>What day is it today? (Que dia é hoje?)</p>
+
+                <p>I work on Wednesday. (Eu trabalho na quarta-feira.)</p>
+
+                <p>The weekend is Saturday and Sunday. (O fim de semana é sábado e domingo.)</p>
+
+                <h4>3. Exemplo de Diálogo:</h4>
+
+                <p>Person A: What day is it today? (Que dia é hoje?) <br>
+                    Person B: Today is Friday. (Hoje é sexta-feira.) <br>
+                    Person A: Do you have plans for the weekend? (Você tem planos para o fim de semana?) <br>
+                    Person B: Yes, I am going to the beach on Saturday. (Sim, eu vou para a praia no sábado.) <br>
+                    Person A: That sounds fun! (Isso parece divertido!)</p>
+                <br><br>
+                <hr>
+                <h2>Exercícios</h2>
+
+                <?php
+
+                $Questions = array(
+                    1 => array(
+                        'Question' => 'Today is _______. (Hoje é segunda.)',
+                        'Answers' => array(
+                            'A' => 'Monday',
+                            'B' => 'Tuesday',
+                            'C' => 'Friday'
+                        ),
+                        'CorrectAnswer' => 'A'
+                    ),
+                    2 => array(
+                        'Question' => 'Tomorrow is _______. (Amanhã é terça-feira.)',
+                        'Answers' => array(
+                            'A' => 'Sunday',
+                            'B' => 'Wednesday',
+                            'C' => 'Tuesday'
+                        ),
+                        'CorrectAnswer' => 'C'
+                    ),
+                    3 => array(
+                        'Question' => 'What day is it today? (Que dia é hoje?)',
+                        'Answers' => array(
+                            'A' => 'It is Monday.',
+                            'B' => 'It is July.',
+                            'C' => 'It is morning.'
+                        ),
+                        'CorrectAnswer' => 'A'
+                    ),
+                    4 => array(
+                        'Question' => 'I work on _______. (Eu trabalho na quarta-feira.)',
+                        'Answers' => array(
+                            'A' => 'Monday',
+                            'B' => 'Wednesday',
+                            'C' => 'Sunday'
+                        ),
+                        'CorrectAnswer' => 'B'
+                    ),
+                    5 => array(
+                        'Question' => 'The weekend is _______. (O fim de semana é ______.)',
+                        'Answers' => array(
+                            'A' => 'Monday and Tuesday',
+                            'B' => 'Wednesday and Thursday',
+                            'C' => 'Saturday and Sunday'
+                        ),
+                        'CorrectAnswer' => 'C'
+                    )
+                );
+
+                if (isset($_POST['answers'])) {
+                    $Answers = $_POST['answers'];
+
+                    foreach ($Questions as $QuestionNo => $Value) {
+
+                        echo $Value['Question'] . '<br />';
+
+                        if ($Answers[$QuestionNo] != $Value['CorrectAnswer']) {
+                            echo 'Você respondeu: <span class="incorrect">' . $Value['Answers'][$Answers[$QuestionNo]] . '</span><br>';
+                            echo 'Resposta correta: <span class="correct">' . $Value['Answers'][$Value['CorrectAnswer']] . '</span>';
+                        } else {
+                            echo 'Resposta correta: <span class="correct">' . $Value['Answers'][$Answers[$QuestionNo]] . '</span><br>';
+                            echo 'Você acertou!';
+                            $counter++;
+                        }
+
+                        echo '<br /><hr>';
+                        if ($counter == "") {
+                            $counter = '0';
+                            $results = "Pontuação: $counter/5";
+                        } else {
+                            $results = "Pontuação: $counter/5";
+                        }
+                    }
+                    echo $results;
+                } else {
+                ?>
+                    <form action="<?php echo $_SERVER['../modulo1/PHP_SELF']; ?>" method="post" id="quiz">
+                        <?php foreach ($Questions as $QuestionNo => $Value) { ?>
+
+                            <p class="pergunta"><?php echo $Value['Question']; ?></p>
+                            <?php
+                            foreach ($Value['Answers'] as $Letter => $Answer) {
+                                $Label = 'question-' . $QuestionNo . '-answers-' . $Letter;
+                            ?>
+                                <div class="opcoes">
+                                    <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter; ?>" />
+                                    <label for="<?php echo $Label; ?>"><?php echo $Letter; ?>) <?php echo $Answer; ?> </label>
+                                </div>
+                            <?php } ?>
+
+                        <?php } ?>
+                        <input type="submit" value="Verificar" class="botao-verificar" />
+                    </form>
+                <?php
+                }
+                ?>
+
+                <div class="proximo">
+                    <p><a href="m2-a4.php" class="proximo-link">Ir para a lição 14</a></p>
+                </div>
+
+            </main>
+        <?php
+
+        }
+
+        mysqli_close($dbc);
+        ?>
+
+        <footer>
+            <p id="copyright">Todos os direitos reservados <script>
+                    document.write(new Date().getFullYear());
+                </script>
+            </p>
+        </footer>
+
+    </div>
+</body>
+
+</html>
